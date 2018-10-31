7a3de18bd276976dc2f1f0009d52d3f36f423293
ClintonClin <clintonclin00@gmail.com>
2018-10-31 08:09:02 +0300

    added functionality to form component

--- src/app/git-forms/git-forms.component.ts
+++ src/app/git-forms/git-forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Http, Response} from '@angular/http';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-git-forms',
   templateUrl: './git-forms.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitFormsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:Http){}
+  userName="";
+  githubData:any="";
+  avatarUrl="./assets/avatar.png";
 
+  userLookup(){
+    this.http.get("https://api.github.com/users/"+this.userName+"?access_token="+environment.access_token)
+    .subscribe(
+      (response:Response)=>{
+        const userData=response.json();
+        this.githubData=userData;
+        this.avatarUrl=userData.avatar_url;
+        console.log(userData);
+      }
+    )
+    }
   ngOnInit() {
   }
 
 }
+
